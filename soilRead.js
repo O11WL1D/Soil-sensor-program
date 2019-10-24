4b5a9ab273b216e9ac9d3a616a89719bfbf4a8d8
entrylevelcs <54680246+entrylevelcs@users.noreply.github.com>
2019-10-24 14:31:37 -0600

    Update soilRead.js

--- soilRead.js
+++ soilRead.js
@@ -1,7 +1,7 @@
 function soilReading() {
     pins.digitalWritePin(DigitalPin.P6, 1)
     basic.pause(50)
-    let soil = pins.digitalReadPin(DigitalPin.P7)
+    let soil = pins.analogReadPin(AnalogPin.P7)
     pins.digitalWritePin(DigitalPin.P6, 0)
     return soil
-} 
+}
